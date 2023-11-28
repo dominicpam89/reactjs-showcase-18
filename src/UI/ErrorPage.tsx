b7809e28b69ecb0eb68ff691445d6ca98fed8499
dominicpam89 <dominicpam89@gmail.com>
2023-11-28 14:49:28 +0700

    Error Page Feature

--- src/UI/ErrorPage.tsx
+++ src/UI/ErrorPage.tsx
@@ -1,7 +1,22 @@
+import { useNavigate } from "react-router-dom"
+import { motion } from "framer-motion"
+
 const UIErrorPage = () => {
-  return <>
-    <h1 className="text-2xl">UIErrorPage</h1>
-  </>
+   const navigate = useNavigate()
+   return (
+      <div className="p-10 flex flex-col justify-content items-center space-y-5">
+         <h1 className="text-4xl font-black text-primary-500">404</h1>
+         <p className="text-sm font-medium flex space-x-2 items-center">
+            <span>Url not found</span>
+            <motion.button
+               onClick={() => navigate(-1)}
+               className="btn btn-sm btn-primary-outlined"
+            >
+               go back
+            </motion.button>
+         </p>
+      </div>
+   )
 }
- 
-export default UIErrorPage;
+
+export default UIErrorPage
